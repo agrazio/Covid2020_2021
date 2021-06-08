b83f8c032f5b88001a64ad4b0c8193ecb9618395
Andrea Grazioli <andreagrazioli@hotmail.it>
2021-06-08 22:14:33 +0200

    Moved to Google Cloud Functions

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 from urllib.request import urlopen
 import json
+import os
 from datetime import datetime, date
+from github import Github
 
-url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json"
-response = urlopen(url)
-data_json = json.loads(response.read())
 
-ricoverati_con_sintomi = [[],[]]
-terapie_intensive = [[],[]]
-totale_positivi = [[],[]]
-nuovi_positivi = [[],[]]
-date = [[],[]]
+def handler(request):
+     url =  os.environ.get('DATA_URL')
+     git = Github(os.environ.get('GIT_KEY'))
+     repo = git.get_repo(os.environ.get('GIT_REPO'))
 
-for value in data_json:
-    data = datetime.fromisoformat(value["data"]).date()
+     response = urlopen(url)
+     data_json = json.loads(response.read())
 
-    index = 0 if data < datetime(2021, 2, 24).date() else 1
+     ricoverati_con_sintomi = [[], []]
+     terapie_intensive = [[], []]
+     totale_positivi = [[], []]
+     nuovi_positivi = [[], []]
+     date = [[], []]
 
-    if data != datetime(2020, 2, 29).date(): # modo poco elegante per superare l'anno bisestile
-        ricoverati_con_sintomi[index].append(value["ricoverati_con_sintomi"])
-        terapie_intensive[index].append(value["terapia_intensiva"])
-        totale_positivi[index].append(value["totale_positivi"])
-        nuovi_positivi[index].append(value["nuovi_positivi"])
-        date[index].append(data.strftime("%Y-%m-%d"))
+     for value in data_json:
+          data = datetime.fromisoformat(value["data"]).date()
 
-output = {}
-output["ricoverati_con_sintomi"] = ricoverati_con_sintomi
-output["terapie_intensive"] = terapie_intensive
-output["totale_positivi"] = totale_positivi
-output["nuovi_positivi"] = nuovi_positivi
-output["date"] = date
+          index = 0 if data < datetime(2021, 2, 24).date() else 1
 
-print(json.dumps(output))
+        # modo poco elegante per superare l'anno bisestile
+          if data != datetime(2020, 2, 29).date():
+               ricoverati_con_sintomi[index].append(
+                value["ricoverati_con_sintomi"])
+          terapie_intensive[index].append(value["terapia_intensiva"])
+          totale_positivi[index].append(value["totale_positivi"])
+          nuovi_positivi[index].append(value["nuovi_positivi"])
+          date[index].append(data.strftime("%Y-%m-%d"))
+
+     output = {}
+     output["ricoverati_con_sintomi"] = ricoverati_con_sintomi
+     output["terapie_intensive"] = terapie_intensive
+     output["totale_positivi"] = totale_positivi
+     output["nuovi_positivi"] = nuovi_positivi
+     output["date"] = date
+     new_data = json.dumps(output)
+     content = repo.get_contents("output.json")
+     repo.update_file(path=content.path, message="Auto update", content=new_data, sha=content.sha, branch="master")
+
+     return f'OK!'
