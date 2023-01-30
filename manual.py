be111256a092344381e42c6e6eabf916e8f901d9
agrazio <41650706+agrazio@users.noreply.github.com>
2023-01-30 23:38:28 +0100

    2023 and data refactor (#3)

--- manual.py
+++ manual.py
@@ -1,58 +1,54 @@
 from urllib.request import urlopen
 import json
-from datetime import datetime, date
-
+from datetime import datetime
 
 url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json"
 
 response = urlopen(url)
 data_json = json.loads(response.read())
 
-def rolling_mean(data, window_size):
-    i = 0
-    moving_averages = []
-    while i < len(data) - window_size + 1:
-        this_window = data[i : i + window_size]
-        window_average = sum(this_window) / window_size
-        moving_averages.append(round(window_average))
-        i += 1
-    return moving_averages
+covid_values = {
+    "2020": {"ric": [], "ter": [], "tot": [], "pos": []},
+    "2021": {"ric": [], "ter": [], "tot": [], "pos": []},
+    "2022": {"ric": [], "ter": [], "tot": [], "pos": []},
+    "2023": {"ric": [], "ter": [], "tot": [], "pos": []}
+}
 
-ricoverati_con_sintomi = [[], [], []]
-terapie_intensive = [[], [], []]
-totale_positivi = [[], [], []]
-nuovi_positivi = [[], [], []]
-date = [[], [], []]
+
+def rolling_mean(array):
+    window = 7
+    n = len(array)
+    rolling_mean_list = []
+    for i in range(n - window + 1):
+        mean = sum(array[i:i + window]) / window
+        rolling_mean_list.append(round(mean))
+    return rolling_mean_list
+
+
+def enrich_data(year, covid_row):
+    covid_values[year]["ric"].append(covid_row["ricoverati_con_sintomi"])
+    covid_values[year]["ter"].append(covid_row["terapia_intensiva"])
+    covid_values[year]["tot"].append(covid_row["totale_positivi"])
+    covid_values[year]["pos"].append(covid_row["nuovi_positivi"])
+
 
 for value in data_json:
     data = datetime.fromisoformat(value["data"]).date()
 
-    index = 0 if data < datetime(2021, 2, 24).date() else 1
-
-    if data < datetime(2021, 2, 24).date():
-        index = 0
-    elif data >= datetime(2021, 2, 24).date() and data < datetime(2022, 2, 24).date():
-        index = 1
-    else:
-        index = 2
-
-    # modo poco elegante per superare l'anno bisestile
+    # bad way to avoid leap year problem
     if data != datetime(2020, 2, 29).date():
-        ricoverati_con_sintomi[index].append(
-            value["ricoverati_con_sintomi"])
-        terapie_intensive[index].append(value["terapia_intensiva"])
-        totale_positivi[index].append(value["totale_positivi"])
-        nuovi_positivi[index].append(value["nuovi_positivi"])
-        date[index].append(data.strftime("%Y-%m-%d"))
+        if data < datetime(2021, 2, 24).date():
+            enrich_data("2020", value)
+        elif datetime(2021, 2, 24).date() <= data < datetime(2022, 2, 24).date():
+            enrich_data("2021", value)
+        elif datetime(2022, 2, 24).date() <= data < datetime(2023, 2, 24).date():
+            enrich_data("2022", value)
+        else:
+            enrich_data("2023", value)
 
+covid_values["2020"]["pos"] = rolling_mean(covid_values["2020"]["pos"])
+covid_values["2021"]["pos"] = rolling_mean(covid_values["2021"]["pos"])
+covid_values["2022"]["pos"] = rolling_mean(covid_values["2022"]["pos"])
+covid_values["2023"]["pos"] = rolling_mean(covid_values["2023"]["pos"])
 
-nuovi_positivi = list(map(lambda x: rolling_mean(x, 7), nuovi_positivi))
-
-output = {}
-output["ricoverati_con_sintomi"] = ricoverati_con_sintomi
-output["terapie_intensive"] = terapie_intensive
-output["totale_positivi"] = totale_positivi
-output["nuovi_positivi"] = nuovi_positivi
-output["date"] = date
-
-print(json.dumps(output))
+print(json.dumps(covid_values))
